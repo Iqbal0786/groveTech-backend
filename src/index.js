dbd38c95d0a69798f951cd6abc39976ae6f900b8
Mohammad Iqbal <iqbal240456@gmail.com>
2022-06-23 20:19:18 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express= require("express");
+const connect =require("./config/db")
+const cors=require("cors");
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+
+app.get("",async(req,res)=>{
+  try {
+    return res.send("Server is live now")
+  } catch (error) {
+    return res.send(error.message)
+  }
+})
+
+const port=process.env.PORT||1212;
+app.listen(port,async()=>{
+    try {
+         await connect();
+         console.log("Listening the port number 1212");
+    } catch (error) {
+        console.log(error.message)
+    }
+})
