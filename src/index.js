583c4891a424be0c760ecf487c3a22cb69788ea1
Mohammad Iqbal <iqbal240456@gmail.com>
2022-06-23 20:30:07 +0530

    added product controller

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const express= require("express");
 const connect =require("./config/db")
+const productController= require("./controllers/product.controller")
 const cors=require("cors");
 const app = express();
 app.use(express.json());
 app.use(cors());
-
+app.use("/v1/products",productController)
 
 
 app.get("",async(req,res)=>{
