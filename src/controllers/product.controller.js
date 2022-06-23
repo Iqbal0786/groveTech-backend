dfedcd834d5ad2b5322db26ed8ec2850c25bcd28
Mohammad Iqbal <iqbal240456@gmail.com>
2022-06-24 01:09:48 +0530

    updated search query

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -5,9 +5,24 @@ const router= express.Router();
 //get router
 router.get("",async(req,res)=>{
     try {
-         const products= await Product.find().lean().exec();
-         return res.status(200).send(products)
-        
+        const page= req.query.page||1
+        const size= req.query.size||10
+        const query={category:req.query.search}
+        if(req.query.search){
+            const products = await Product.find(query) 
+            .skip((page - 1) * size)
+            .limit(size).sort({createdAt:-1})
+            .lean()
+            .exec();
+         
+          const totalPages = Math.ceil(
+            (await Product.find(query).countDocuments()) / size);
+      
+          return res.send({ products, totalPages });
+        }
+        const products= await Product.find().lean().exec();
+        return res.status(200).send(products)
+       
     } catch (error) {
         return res.status(500).send({message:error.message})
     }
